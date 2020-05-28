a6a81e2599aa94720a303904770446c8ccc2b77f
Anatoliy Gaysenko <a.gaysenko@gmail.com>
2020-05-28 00:35:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,9 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
 introduction("Josh")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
-
 introduction_with_language("Dan", "Ember.")
